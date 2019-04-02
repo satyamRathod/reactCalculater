6c3db1c4a8cd5c70800377ead646180274883703
Satyam Rathod <satyam.rathod@synerzip.com>
2019-04-03 00:51:59 +0530

    initial Commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,18 @@ import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-export default class Home extends Component {
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  }
+});
+
+class Home extends Component {
   state = {
     numArray: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+", "-", "/", "*"],
     result: "",
@@ -18,7 +29,6 @@ export default class Home extends Component {
 
   handleClick = e => {
     const result = eval(this.state.result);
-
     this.setState({
       result: result
     });
@@ -32,113 +42,101 @@ export default class Home extends Component {
   render() {
     const { input, result, numArray } = this.state;
     return (
-      <div style={{ width: "250px", marginLeft: "80px", marginTop: "100px" }}>
-        <Card style={{ backgroundColor: "#ccc", height: "350px" }}>
-          <Card.Body>
-            <Row>
+      <>
+        <div style={{ width: "250px", marginLeft: "80px", marginTop: "100px" }}>
+          <Card style={{ backgroundColor: "#ccc", height: "350px" }}>
+            <Card.Body>
+              <Row>
               <input
                 type="text"
                 onChange={this.handleChange}
                 value={result}
                 style={{ width: "100%", height: "100px" }}
               />
-            </Row>
-            <Row>&nbsp;</Row>
-            <Row>
-              <Col lg={6}>
-                <tr>
-                  {[0, 1, 2, 3].map(e => (
-                    <td>
-                      <div
-                        style={{
-                          border: "1px solid",
-                          padding: "2px 4px",
-                          margin: "2px 4px"
-                        }}
-                        onClick={() => this.handleButtonKey(e)}
-                      >
-                        {e}
-                      </div>
-                    </td>
-                  ))}
-                </tr>
-                <tr>
-                  {[0, 1, 2, 3].map(e => (
-                    <td>
-                      <div
-                        style={{
-                          border: "1px solid",
-                          padding: "2px 4px",
-                          margin: "2px 4px"
-                        }}
-                        onClick={() => this.handleButtonKey(e)}
-                      >
-                        {e}
-                      </div>
-                    </td>
-                  ))}
-                </tr>
-                <tr>
-                  {[4, 5, 6, 7].map(e => (
-                    <td>
-                      <div
-                        style={{
-                          border: "1px solid",
-                          padding: "2px 4px",
-                          margin: "2px 4px"
-                        }}
-                        onClick={() => this.handleButtonKey(e)}
-                      >
-                        {e}
-                      </div>
-                    </td>
-                  ))}
-                </tr>
-                <tr>
-                  {[8, 9, "+", "-"].map(e => (
-                    <td>
-                      <div
-                        style={{
-                          border: "1px solid",
-                          padding: "2px 4px",
-                          margin: "2px 4px"
-                        }}
-                        onClick={() => this.handleButtonKey(e)}
-                      >
-                        {e}
-                      </div>
-                    </td>
-                  ))}
-                </tr>
-                <tr>
-                  {["*", "/"].map(e => (
-                    <td>
-                      <div
-                        style={{
-                          border: "1px solid",
-                          padding: "2px 4px",
-                          margin: "2px 4px"
-                        }}
-                        onClick={() => this.handleButtonKey(e)}
-                      >
-                        {e}
-                      </div>
-                    </td>
-                  ))}
-                </tr>
-              </Col>
-              <Col>
-                <button
-                  onClick={this.handleClick}
-                  style={{ width: "100%", height: "150px" }}
-                >
-                  =
-                </button>
-              </Col>
-            </Row>
-          </Card.Body>
-        </Card>
-      </div>
+              </Row>
+              <Row>&nbsp;</Row>
+              <Row>
+                <Col lg={6}>
+                  <tr>
+                    {[0, 1, 2, 3].map(e => (
+                      <td>
+                        <div
+                          style={{
+                            border: "1px solid",
+                            padding: "2px 4px",
+                            margin: "2px 4px"
+                          }}
+                          onClick={() => this.handleButtonKey(e)}
+                        >
+                          {e}
+                        </div>
+                      </td>
+                    ))}
+                  </tr>
+
+                  <tr>
+                    {[4, 5, 6, 7].map(e => (
+                      <td>
+                        <div
+                          style={{
+                            border: "1px solid",
+                            padding: "2px 4px",
+                            margin: "2px 4px"
+                          }}
+                          onClick={() => this.handleButtonKey(e)}
+                        >
+                          {e}
+                        </div>
+                      </td>
+                    ))}
+                  </tr>
+                  <tr>
+                    {[8, 9, "+", "-"].map(e => (
+                      <td>
+                        <div
+                          style={{
+                            border: "1px solid",
+                            padding: "2px 4px",
+                            margin: "2px 4px"
+                          }}
+                          onClick={() => this.handleButtonKey(e)}
+                        >
+                          {e}
+                        </div>
+                      </td>
+                    ))}
+                  </tr>
+                  <tr>
+                    {["*", "/"].map(e => (
+                      <td>
+                        <div
+                          style={{
+                            border: "1px solid",
+                            padding: "2px 4px",
+                            margin: "2px 4px"
+                          }}
+                          onClick={() => this.handleButtonKey(e)}
+                        >
+                          {e}
+                        </div>
+                      </td>
+                    ))}
+                  </tr>
+                </Col>
+                <Col>
+                  <button
+                    onClick={this.handleClick}
+                    style={{ width: "100%", height: "150px" }}
+                  >
+                    =
+                  </button>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </div>
+      </>
     );
   }
 }
+export default Home;
